4edb9abff700af56f94c6c6bb5a108d935d25eb7
Espen.Kalvatn <Espen.Kalvatn@student.uib.no>
2022-09-02 11:39:09 +0200

    Using the PepCheckService to check if the given name exists in the list.

--- src/main/java/Stacc/KYC/API/controller/PepCheckController.java
+++ src/main/java/Stacc/KYC/API/controller/PepCheckController.java
@@ -1,5 +1,7 @@
 package Stacc.KYC.API.controller;
 
+import Stacc.KYC.API.service.PepCheckService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +13,13 @@ import java.io.IOException;
 @RequestMapping("/check")
 public class PepCheckController {
 
+    @Autowired
+    PepCheckService pepCheckService;
 
     @RequestMapping("person/{name}")
     @ResponseBody
     public String checkPerson(@PathVariable String name) throws IOException {
-        return name;
+        return pepCheckService.checkPerson(name);
     }
 
     @RequestMapping("organization/{orgNumber}")
